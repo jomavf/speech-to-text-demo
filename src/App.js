62a70e2de9eee6a1af5cb6b712c3b7e7f77ba787
ZetaGH <jose.veliz.francia@gmail.com>
2019-10-16 18:08:17 -0500

    modificacion de firebase.json y home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Switch, Redirect, Route } from 'react-router'
 
 import HomePage from './Containers/Home'
@@ -12,15 +12,17 @@ class App extends Component {
   render(){
     return (
       <BrowserRouter>
-        <NavBar/>
-        <main>
-          <Switch>
-            <Redirect from="/" to="/search" exact/>
-            <Route path="/home" component={HomePage}/>
-            <Route path="/search" component={SearchPage}/>
-            <Route path="/record" component={RecordPage}/>
-          </Switch>
-        </main>
+        <React.Fragment>
+          <NavBar/>
+          <main>
+            <Switch>
+                <Redirect from="/" to="/home" exact/>
+                <Route path="/home" component={HomePage}/>
+                <Route path="/search" component={SearchPage}/>
+                <Route path="/record" component={RecordPage}/>
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>  
     );
   }
--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -4,8 +4,26 @@ import './index.css'
 
 const Home = () => {
     return (
-        <div class="home__main">
+        <div className="home__main">
             <h1>Sumarización de eventos en video consultados mediante el uso de speech recognition</h1>
+            <div className="home__description">
+                <h2>Descripción del proyecto</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non repellat architecto odit voluptate, minima obcaecati nulla beatae accusantium earum dolorem accusamus quo consectetur quidem commodi, dolore eum! Voluptate, dignissimos eius!</p>
+            </div>
+            <hr/>
+            <div className="home__content">
+                <h2>Contenido</h2>
+                <div className="home__content_card">
+                    <div className="home__card">
+                        <h3>Buscar</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta repellat nulla nam error non amet veritatis esse obcaecati optio consectetur.</p>
+                    </div>
+                    <div className="home__card">
+                        <h3>Grabar</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta repellat nulla nam error non amet veritatis esse obcaecati optio consectetur.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
